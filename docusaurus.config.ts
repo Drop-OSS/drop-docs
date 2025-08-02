ee59b74af1abdf1e8c2fe89a16d2ada19bec185c
quexeky <git@quexeky.dev>
2025-08-02 22:34:56 +1000

    chore: add cloudflare-pr-testing workflow
    Signed-off-by: quexeky <git@quexeky.dev>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: "/drop.svg",
 
   // Set the production url of your site here
-  url: "https://docs.droposs.org",
+  url: "BASE_URL",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
